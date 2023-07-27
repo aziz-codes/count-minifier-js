b0c07798305e4154e33b8de573d7bd6ce334b7ef
aziz-codes <azizisoffline@gmail.com>
2023-07-27 22:02:45 +0500

    defined dummy range for 10K,100K,1M,10M etc

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const countMinifier = (count) => {
   let kth = count / 1000;
+  let tenKth = count / 10000;
+  let lacKth = count / 100000;
+  let millKth = count / 1000000;
+  let tenMlth = count / 10000000;
 
   if (count % 2 === 0) return kth + "k";
   return kth.toString().slice(0, -4) + "k";
